de206c4fec35ac64ace5601b09ce285a25ba1adc
rfuke <rfuke@loffacorp.com>
2021-09-19 23:39:42 -0700

    add about me page

---
+++ src/components/About/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+function About() {
+
+  return (
+    
+    <section id="AboutMe" class="flex-container">
+    <div class="my-title-flex">
+      <h3> About Me</h3>
+  </div>
+    <div class="my-right-flex">
+      <p>
+      IT expert newly located to the US for lifestyle change and living in New York City (Woodhaven) as a 
+      permanent resident (Green Card holder). Married with one child. Fluent in English.  
+      </p>
+      <p>
+      Advanced  knowledge  of  the  Server  operating  systems  (Windows NT/2000/2003/2008/2012,  Linux 
+      CentOS  5/6),  client  operating  systems  (Windows,  Linux, Mac),  advanced  usage  of MS-OFFICE, 
+      different software applications packages, broad knowledge of networks, communications protocols, IT 
+      hardware,  communication  technologies,  large  scale  experience with  state  of  the  art  brand  name  IT 
+      hardware  components;  skilled  in  review  and  evaluation  of  computer  networks  and  deployment  of 
+      modifications for improvements. 
+      </p>
+      <p>
+      Twenty years of professional experience  in computer hardware and  software  systems  (management, 
+      evaluation, planning, integration) including computer networks; team leading in ICT (Information and 
+      Communication  Technologies)  system  integrators  and  ICT  service  providers;  experience  with 
+      implementation of EC funded contract.
+      </p>
+    </div>
+  </section>
+  )
+}
+
+
+
+export default About
